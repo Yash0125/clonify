5486527bf710862468c6eb09e13cc70b8577d8a1
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-03-26 16:48:12 +0530

    feat : Header added

--- src/components/BodyContent/BodyContent.jsx
+++ src/components/BodyContent/BodyContent.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './BodyContent.css'
+import Header from '../Header/Header';
 
 const BodyContent = () => {
   return (
     <div className='bodycontent-container'>
-      
+      <Header/>
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Header.css";
+import { CiSearch } from "react-icons/ci";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { CiCalendar } from "react-icons/ci";
+import { AiOutlineAppstore } from "react-icons/ai";
+import profileImg from "./../../assets/Pasteimage.png";
+import Logo from "./../../assets/Branding.png";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header-container">
+        <div className="input-container">
+          <CiSearch className="search-icon" />
+          <input className="search-input" type="text" placeholder="Search..." />
+        </div>
+
+        <div className="header-right-container">
+          <div className="header-right-icon-container">
+            <div className="notification-container">
+              <IoIosNotificationsOutline className="header-right-icon" />
+              <div className="notification-icon"></div>
+            </div>
+            <div className="notification-container">
+              <CiCalendar className="header-right-icon" />
+              <AiOutlineAppstore className="header-right-icon" />
+            </div>
+          </div>
+          <div className="header-right-profile-container">
+            <img className="profile-image" src={profileImg} alt="profile-img" />
+            <div className="notification-profile-icon"></div>
+          </div>
+        </div>
+      </header>
+      <header className="header-mobile-container">
+        <div className="header-mobile-details-container">
+          <div className="clonify-logo-container">
+            <img
+              className="clonify-logo-header"
+              src={Logo}
+              alt="Clonify-logo"
+            />
+          </div>
+          <div className="header-right-profile-container">
+            <img className="profile-image" src={profileImg} alt="profile-img" />
+            <div className="notification-profile-icon"></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
